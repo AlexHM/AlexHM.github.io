805232e5ca5e8812274211289ddb82fb2ae1992c
AlexHM <alejandro.honrubia@live.u-tad.com>
2021-10-23 22:32:57 +0200

    Continue with home and scrollReveal

---
+++ source.js
@@ -0,0 +1,47 @@
+
+window.sr = ScrollReveal();
+
+
+
+
+//Content
+sr.reveal('#titleContent',{
+    duration:2000,
+    origin:'left',
+    distance:'100px'
+});
+sr.reveal('#infoContent',{
+    duration:2000,
+    origin:'bottom',
+    distance:'100px'
+});
+
+//PersonalInfo
+sr.reveal('#personalText',{
+    duration:2000,
+    origin:'left',
+    distance:'100px'
+});
+sr.reveal('#personalImage',{
+    duration:10000,
+    origin:'bottom',
+    distance:'100px'
+});
+
+
+sr.reveal('#tittleSkills',{
+    duration:2000,
+    origin:'left',
+    distance:'100px'
+});
+sr.reveal('.skillsImg',{
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+});
+
+
+
+
+
+
