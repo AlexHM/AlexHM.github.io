869f9eccb9cb78c165cadf8af072e8044410f51d
AlexHM <alejandro.honrubia@live.u-tad.com>
2021-10-27 19:07:53 +0200

    Added some efects into Projects.html and css

--- project.js
+++ project.js
@@ -26,3 +26,46 @@ sr.reveal('#p5',{
     origin:'bottom',
     distance:'400px'
 });
+
+
+
+let select = document.getElementById('selector1');
+let option1 = document.getElementById("p1");
+let option2 = document.getElementById("p2");
+let option3 = document.getElementById("p3");
+let option4 = document.getElementById("p4");
+let option5 = document.getElementById("p5");
+
+select.addEventListener('change',
+  function(){
+    let selectedOption = select.value;
+    console.log(selectedOption);
+    
+    if (selectedOption == "On Creation") {
+        option1.style.display= "none";
+        option2.style.display= "none";
+        option3.style.display= "none";
+        option4.style.display= "block";
+        option5.style.display= "block";
+        
+
+    }else if (selectedOption == "Finished") {
+        option4.style.display= "none";
+        option5.style.display= "none";
+        option1.style.display= "block";
+        option2.style.display= "block";
+        option3.style.display= "block";
+    }else{
+        option1.style.display= "block";
+        option2.style.display= "block";
+        option3.style.display= "block";
+        option4.style.display= "block";
+        option5.style.display= "block";
+        
+    }
+    
+  });
+
+  function comingSoon() {
+      alert("Coming soon...");
+  }
