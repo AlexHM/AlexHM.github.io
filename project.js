915baf3a2f7ddf4e7b83f5fe560f4d0a46f4a909
AlexHM <alejandro.honrubia@live.u-tad.com>
2022-01-04 12:33:21 +0100

    Added educational game v2 SpaceAdventure

--- project.js
+++ project.js
@@ -1,40 +1,38 @@
 window.sr = ScrollReveal();
 //Projects divs
 
-sr.reveal('#p1',{
-    duration:4000,
-    origin:'right',
-    distance:'400px'
+sr.reveal("#p1", {
+  duration: 4000,
+  origin: "right",
+  distance: "400px",
 });
-sr.reveal('#p2',{
-    duration:3000,
-    origin:'right',
-    distance:'300px'
+sr.reveal("#p2", {
+  duration: 3000,
+  origin: "right",
+  distance: "300px",
 });
-sr.reveal('#p3',{
-    duration:2000,
-    origin:'right',
-    distance:'200px'
+sr.reveal("#p3", {
+  duration: 2000,
+  origin: "right",
+  distance: "200px",
 });
-sr.reveal('#p4',{
-    duration:1000,
-    origin:'right',
-    distance:'100px'
+sr.reveal("#p4", {
+  duration: 1000,
+  origin: "right",
+  distance: "100px",
 });
-sr.reveal('#p5',{
-    duration:5000,
-    origin:'bottom',
-    distance:'400px'
+sr.reveal("#p5", {
+  duration: 5000,
+  origin: "bottom",
+  distance: "400px",
 });
-sr.reveal('#p6',{
-    duration:5000,
-    origin:'bottom',
-    distance:'400px'
+sr.reveal("#p6", {
+  duration: 5000,
+  origin: "bottom",
+  distance: "400px",
 });
 
-
-
-let select = document.getElementById('selector1');
+let select = document.getElementById("selector1");
 let option1 = document.getElementById("p1");
 let option2 = document.getElementById("p2");
 let option3 = document.getElementById("p3");
@@ -42,38 +40,32 @@ let option4 = document.getElementById("p4");
 let option5 = document.getElementById("p5");
 let option6 = document.getElementById("p6");
 
-select.addEventListener('change',
-  function(){
-    let selectedOption = select.value;
-    console.log(selectedOption);
-    
-    if (selectedOption == "On Creation") {
-        option1.style.display= "none";
-        option2.style.display= "none";
-        option3.style.display= "none";
-        option4.style.display= "block";
-        option5.style.display= "block";
-        option6.style.display= "none";
-        
+select.addEventListener("change", function () {
+  let selectedOption = select.value;
 
-    }else if (selectedOption == "Finished") {
-        option4.style.display= "none";
-        option5.style.display= "none";
-        option1.style.display= "block";
-        option2.style.display= "block";
-        option3.style.display= "block";
-        option6.style.display= "block";
-    }else{
-        option1.style.display= "block";
-        option2.style.display= "block";
-        option3.style.display= "block";
-        option4.style.display= "block";
-        option5.style.display= "block";
-        
-    }
-    
-  });
-
-  function comingSoon() {
-      alert("Coming soon...");
+  if (selectedOption == "On Creation") {
+    option1.style.display = "none";
+    option2.style.display = "none";
+    option3.style.display = "none";
+    option4.style.display = "block";
+    option5.style.display = "none";
+    option6.style.display = "none";
+  } else if (selectedOption == "Finished") {
+    option4.style.display = "none";
+    option5.style.display = "block";
+    option1.style.display = "block";
+    option2.style.display = "block";
+    option3.style.display = "block";
+    option6.style.display = "block";
+  } else {
+    option1.style.display = "block";
+    option2.style.display = "block";
+    option3.style.display = "block";
+    option4.style.display = "block";
+    option5.style.display = "block";
   }
+});
+
+function comingSoon() {
+  alert("Coming soon...");
+}
